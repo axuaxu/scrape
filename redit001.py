af4f0f2af9c59cd27725992e9d35dfc7b97f8073
axu <axu0110@gmail.com>
2015-02-16 23:02:58 -0500

    ff

--- redit001.py
+++ redit001.py
@@ -8,6 +8,8 @@ domain = tree.xpath('//span[@class="domain"]/a/text()')
 submitter = tree.xpath('//p[@class="tagline"]/a/text()')
 liFirst = tree.xpath('//li[@class="first"]/a/text()')
 FullName = tree.xpath('//@data-fullname')
+nextprev = tree.xpath('//span[@class="nextprev"]/a/@href')
+#lin = nextprev[0].text
 #code = tree.findtext('thing id-')
 #This will create a list of prices
 #prices = tree.xpath('//span[@class="item-price"]/text()')
@@ -16,4 +18,5 @@ print '\ndomain: ', domain
 print '\nsubmitter: ', submitter
 print '\nliFirst: ', liFirst
 print '\nFullName: ', FullName
+print '\nnextprev: ', nextprev
 #print 'Prices: ', prices
